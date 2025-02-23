afde1b3ba78cd5031a2582f2d8ba0e2bd1b7fd51
Pavel-rud <pahaprimorye2015@gmail.com>
2025-02-23 19:04:28 +1000

    feat: add user methods in db controller

--- dpns/db/connector.py
+++ dpns/db/connector.py
@@ -13,6 +13,86 @@ from dpns.db.models.problem_author import ProblemAuthor
 
 
 class Controller():
+    """ Users """
+
+    @staticmethod
+    async def create_user(login: str | None = None, tg_id: str | None = None, role: str = "user"):
+        session: AsyncSession = await get_session()
+        if login is None and tg_id is None:
+            return "User must have tg_id or login"
+        user = User(login=login, tg_id=tg_id, role=role)
+        session.add(user)
+        await session.commit()
+
+    @staticmethod
+    async def get_user(user_id: int | None = None, login: str | None = None, tg_id: str | None = None):
+        session: AsyncSession = await get_session()
+        if [user_id, login, tg_id].count(None) == 2:
+            return "You should choose one parameter: user_id, login or tg_id"
+        if user_id is not None:
+            user = (await session.execute(select(User).filter(User.id == user_id))).scalar()
+        if login is not None:
+            user = (await session.execute(select(User).filter(User.login == login))).scalar()
+        if tg_id is not None:
+            user = (await session.execute(select(User).filter(User.tg_id == tg_id))).scalar()
+        return user
+
+    @staticmethod
+    async def change_role(new_role: str, user_id: int | None = None, login: str | None = None, tg_id: str | None = None):
+        session: AsyncSession = await get_session()
+        if [user_id, login, tg_id].count(None) == 2:
+            return "You should choose one parameter: user_id, login or tg_id"
+        if user_id is not None:
+            user = (await session.execute(select(User).filter(User.id == user_id))).scalar()
+        if login is not None:
+            user = (await session.execute(select(User).filter(User.login == login))).scalar()
+        if tg_id is not None:
+            user = (await session.execute(select(User).filter(User.tg_id == tg_id))).scalar()
+        user.role = new_role
+        await session.commit()
+
+    @staticmethod
+    async def delete_user(user_id: int | None = None, login: str | None = None, tg_id: str | None = None):
+        session: AsyncSession = await get_session()
+        if [user_id, login, tg_id].count(None) == 2:
+            return "You should choose one parameter: user_id, login or tg_id"
+        if user_id is not None:
+            await session.delete(select(User).filter(User.id == user_id))
+        if login is not None:
+            await session.delete(select(User).filter(User.login == login))
+        if tg_id is not None:
+            await session.delete(select(User).filter(User.tg_id == tg_id))
+        await session.commit()
+
+    @staticmethod
+    async def connect_tg(tg_id: str | None = None, user_id: int | None = None, login: str | None = None):
+        session: AsyncSession = await get_session()
+        if [user_id, login].count(None) == 1:
+            return "You should choose one parameter: user_id or login"
+        if user_id is not None:
+            user = (await session.execute(select(User).filter(User.id == user_id))).scalar()
+        if login is not None:
+            user = (await session.execute(select(User).filter(User.login == login))).scalar()
+        if user.tg_id is not None:
+            return "User already connect tg"
+        user.tg_id = tg_id
+        await session.delete(select(User).filter(User.tg_id == tg_id))  # delete old tg id user
+        await session.commit()
+
+    @staticmethod
+    async def delete_tg(user_id: int | None = None, login: str | None = None):
+        session: AsyncSession = await get_session()
+        if [user_id, login].count(None) == 1:
+            return "You should choose one parameter: user_id or login"
+        if user_id is not None:
+            user = (await session.execute(select(User).filter(User.id == user_id))).scalar()
+        if login is not None:
+            user = (await session.execute(select(User).filter(User.login == login))).scalar()
+        if user.tg_id is None:
+            return "User didn't connected tg"
+        user.tg_id = None
+        await session.commit()
+
     """ Tokens """
 
     @staticmethod
@@ -51,7 +131,9 @@ class Controller():
 controller = Controller()
 
 # test
-# import asyncio
+import asyncio
+asyncio.run(controller.create_user(login="lol647"))
+asyncio.run(controller.delete_user(login="lol555"))
 # print(asyncio.run(controller.add_token("lol", "admin")))
 # print(asyncio.run(controller.check_token("4c10953948de466caaac1b98619ec8b1")))
 # print(asyncio.run(controller.del_token("lol")))
