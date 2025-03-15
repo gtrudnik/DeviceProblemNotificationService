63922ad82a0733e3ae659458cb75c35029782e2c
Pavel-rud <pahaprimorye2015@gmail.com>
2025-03-15 23:09:08 +1000

    feat: add home page

---
+++ frontend/dpns/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="text-center">Домашняя страница</h1>
+</template>
+
+<script>
+
+import axios from 'axios';
+export default {
+  name: '',
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin-top: 100px;
+}
+</style>
--- frontend/dpns/src/router/router.js
+++ frontend/dpns/src/router/router.js
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CreateProblem from '../components/CreateProblem.vue'
 import AuthForm from '../components/AuthForm.vue'
 import UserProblems from '../components/UserProblems.vue'
+import Home from '../components/Home.vue'
 
 const routes = [
+  {path: '/', component: Home,},
   {path: '/auth_form', component: AuthForm,},
   {path: '/create_problem', component: CreateProblem,},
   {path: '/user_problems', component: UserProblems,},
