17c60184a40cfdeb9b4bd7d4776e01ededa57fa6
Pavel-rud <pahaprimorye2015@gmail.com>
2025-03-28 00:37:47 +1000

    feat: add table tg_codes in db

---
+++ dpns/db/models/tg_code.py
@@ -0,0 +1,18 @@
+from dpns.db.db import Base
+from sqlalchemy import Column, String, DateTime, BigInteger, Integer, ForeignKey
+from datetime import datetime
+
+
+class TgCode(Base):
+    __tablename__ = 'tg_codes'
+    id = Column(
+        BigInteger,
+        nullable=False,
+        unique=True,
+        primary_key=True,
+        autoincrement=True
+    )
+    tg_id = Column(BigInteger, ForeignKey('tg_chats.id'), unique=True)
+    code = Column(Integer)
+    attempts = Column(Integer, default=0)
+    date_created = Column(DateTime, default=datetime.utcnow)
